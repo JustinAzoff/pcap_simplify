416c21f91c2564ec11fc0a52c92c78eac430afb3
Justin Azoff <justin.azoff@gmail.com>
2020-01-21 11:19:49 -0500

    works?

--- to_pcap.go
+++ to_pcap.go
@@ -7,7 +7,11 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"net"
 	"os"
+	"os/exec"
+	"syscall"
+	"time"
 )
 
 var MAGIC = []byte("\x01PKT")
@@ -29,26 +33,64 @@ func NewBufferSplitter(data []byte) (*BufferSplitter, error) {
 	return &BufferSplitter{data}, nil
 }
 
-func (b *BufferSplitter) Next() ([]byte, error) {
+func (b *BufferSplitter) Next() (bool, []byte, error) {
 	if len(b.data) == 0 {
-		return []byte{}, io.EOF
+		return false, []byte{}, io.EOF
 	}
 	b.data = b.data[len(MAGIC):]
 	is_orig := (b.data[0] == FLOW_ORIG)
-	fmt.Printf("Next byte is %d. is_orig=%v\n", b.data[0], is_orig)
+	//fmt.Printf("Next byte is %d. is_orig=%v\n", b.data[0], is_orig)
 	//skip is_orig
 	b.data = b.data[1:]
 	end := bytes.Index(b.data, MAGIC)
 	if end == -1 {
 		end = len(b.data)
 	}
-	fmt.Printf("end is %d\n", end)
+	//fmt.Printf("end is %d\n", end)
 	payload := b.data[:end]
 	b.data = b.data[end:]
-	return payload, nil
+	return is_orig, payload, nil
 }
 
-func expand(r io.Reader, outputFilename string) (int, error) {
+func server(port int, pktchan <-chan []byte) error {
+	l, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", port))
+	if err != nil {
+		return err
+	}
+	log.Printf("Listening on port %d", port)
+	conn, err := l.Accept()
+	if err != nil {
+		return err
+	}
+	log.Printf("Got connection")
+	go io.Copy(ioutil.Discard, conn)
+	for msg := range pktchan {
+		log.Printf("Server writing %d bytes", len(msg))
+		_, err := conn.Write(msg)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+func client(port int, pktchan <-chan []byte) error {
+	conn, err := net.Dial("tcp", fmt.Sprintf("127.0.0.1:%d", port))
+	if err != nil {
+		return err
+	}
+	log.Printf("Connected!")
+	go io.Copy(ioutil.Discard, conn)
+	for msg := range pktchan {
+		log.Printf("Client writing %d bytes", len(msg))
+		_, err := conn.Write(msg)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func expand(r io.Reader, outputFilename string, port int) (int, error) {
 	//just slurp it up
 	data, err := ioutil.ReadAll(r)
 	if err != nil {
@@ -59,18 +101,48 @@ func expand(r io.Reader, outputFilename string) (int, error) {
 		return 0, err
 	}
 	totalPackets := 0
-	for len(data) > 0 {
-		//skip magic
-		payload, err := b.Next()
+	cmd := exec.Command("tcpdump", "-i", "lo", "-w", outputFilename, fmt.Sprintf("port %d", port))
+	err = cmd.Start()
+	if err != nil {
+		return 0, err
+	}
+
+	serverPkts := make(chan []byte)
+	clientPkts := make(chan []byte)
+
+	go func() {
+		err := server(port, serverPkts)
+		if err != nil {
+			log.Printf("Server error: %v", err)
+		}
+	}()
+	time.Sleep(1 * time.Second)
+	go func() {
+		err := client(port, clientPkts)
+		if err != nil {
+			log.Printf("Client error: %v", err)
+		}
+	}()
+
+	for {
+		is_orig, payload, err := b.Next()
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
 			return totalPackets, err
 		}
-		totalPackets ++
-		fmt.Printf("Data is %q\n", payload)
+		totalPackets++
+		//log.Printf("is_orig %v Data is %d bytes\n", is_orig, len(payload))
+		if is_orig {
+			clientPkts <- payload
+		} else {
+			serverPkts <- payload
+		}
+		time.Sleep(100 * time.Millisecond)
 	}
+	cmd.Process.Signal(syscall.SIGINT)
+	cmd.Wait()
 
 	return totalPackets, nil
 }
@@ -93,10 +165,10 @@ func main() {
 	}
 	defer inf.Close()
 
-	packets, err := expand(inf, output)
+	packets, err := expand(inf, output, 443)
 
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	fmt.Printf("%d packets rewritten\n", packets)
+	log.Printf("%d packets rewritten", packets)
 }
